424eb95b9e64dc8f04ae6a35ed11d66d05f71d35
vikir342 <vikiretec@gmail.com>
2025-08-29 10:23:03 -0300

    adicionado construtores

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,25 @@
 
 public class ClasseAutomotores {
-
+	  String corAt;
+	  String marcaAt;
+	  String modeloAt;
+	  String tipoAt;
+	  
+	  public ClasseAutomotores() {
+		  
+	  }
+	  public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		  this.corAt = corPar;
+		  this.marcaAt = marcaPar;
+		  this.modeloAt = modeloPar;
+		  this.tipoAt = tipoPar;
+	  }
+	  //getters
+	  public String pegarCor() {
+		  return corAt;
+	  }
+	  //setters
+	  public void alterarCor(String corPar) {
+		  this.corAt = corPar;
+	  }
 }
--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -3,7 +3,6 @@ public class ClassePrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
