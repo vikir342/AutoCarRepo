270101679c5c01b9b57a8d0e347006dd1cae2f74
vikir342 <vikiretec@gmail.com>
2025-08-29 10:38:18 -0300

    Finalizando parte 1

---
+++ src/ClasseCaminhoes.java
@@ -0,0 +1,10 @@
+
+public class ClasseCaminhoes extends ClasseAutomotores{
+	float cargaSuportadaAt;
+	
+	 public ClasseCaminhoes(String corAt, String marcaAt, String modeloAt, String tipoAt, int cargaSuportadaAt) {
+	        super(corAt, marcaAt, modeloAt, tipoAt);
+	        this.cargaSuportadaAt = cargaSuportadaAt;
+	    }
+	 
+}
